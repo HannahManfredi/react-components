53615aa45c116bd3f9a90ae55d7aa9cdfbee1839
Hannah Manfredi <hmanfredi@gmail.com>
2021-02-02 14:47:46 -0800

    Complete Handling User Events

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+function GroceryListItem(props) {
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The item was clicked.');
+  }
+
+  return (
+    <li onClick={handleClick}>{props.item}
+    </li>
+  );
+
+  };
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => {
+      return (
+        <GroceryListItem key={item.id} item={item} />
+      );
+    })}
+  </ul>
+);
+
+var App = () => (
+  <div>hello world
+    <h2>Hannah's Grocery List</h2>
+    <GroceryList items={['bananas', 'milk', 'sausage', 'pizza', 'cucumbers']}/>
+  </div>
+
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
